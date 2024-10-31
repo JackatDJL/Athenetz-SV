23b8fd37ebd63074a2a089fdc21198189d1b5541
DJL - Jack <71508487+JackatDJL@users.noreply.github.com>
2024-10-31 21:37:00 +0100

    Trying to add Flags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 "use client";
 
 import UnderConstruction from "§comp/Homepage/UnderConstruction";
+import LoggedOut from "§comp/Homepage/LoggedOut";
+import LoggedIn from "§comp/Homepage/LoggedIn";
+import { gk_Main } from "§api/flags";
+import { fbauth } from "§api/firebase/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-export default function Home() {
-  return (
-    <UnderConstruction />
-  );
+
+export default async function Decide() {
+  const gkMain = await gk_Main();
+  async function userexists(): Promise<boolean> {
+    const [user, loading, error] = useAuthState(fbauth);
+    if (user) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  if (!gkMain) {
+    return <UnderConstruction />;
+  } else {
+    if (await userexists()) {
+      return <LoggedIn />;
+    } else {
+      return <LoggedOut />;
+    }
+  }
 }
