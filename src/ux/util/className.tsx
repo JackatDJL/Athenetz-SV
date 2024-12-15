7c6e4ea3f9237b08bb1c20d9b7f17e6c332486ce
DJL - Jack <71508487+JackatDJL@users.noreply.github.com>
2024-12-15 19:26:06 +0100

    Switched to my Package

--- src/ux/util/className.tsx
+++
@@ -1,6 +0,0 @@
-
-export const DefaultWrapper: string = `oxanium bg-l-bg dark:bg-d-bg text-l-txt dark:text-d-txt duration-700`;
-
-export const DefaultText: string = `oxanium text-l-txt dark:text-d-txt duration-700`;
-
-export const DefaultHeader: string = `gigalypse text-l-txt dark:text-d-txt duration-700 mb-4 text-2xl font-bold`;
