0e3169f98a6daafbcf6d7edcc64ea20ae2203729
DJL - Jack <71508487+JackatDJL@users.noreply.github.com>
2025-01-02 17:37:37 +0100

    Api Working as Expected

--- src/app/api/wahlen/manage/route.ts
+++ src/app/api/wahlen/manage/route.ts
@@ -1,140 +1,107 @@
-import { Account, Client, Databases, ID, Teams } from "appwrite";
+import { Account, Client, Databases, ID, Query, Teams } from "appwrite";
 
-export async function GET() {
-  console.log("GET request received");
-  return Response.json({}, { status: 501 });
+export async function GET(request: Request) {
+  const data = await request.headers;
+  const client = new Client()
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject("athesv")
+    .setJWT(data.get("token") || "");
+  const teams = new Teams(client);
+
+  try {
+    const teamcheck = await teams.list();
+    if (
+      teamcheck.teams.some(
+        (team) => team.$id !== "admin" && team.$id !== "committee"
+      )
+    ) {
+      return Response.json({}, { status: 401 });
+    }
+
+    const pollid = data.get("pollid");
+    const db = new Databases(client);
+    const user = await new Account(client).get();
+
+    if (pollid) {
+      return Response.json({}, { status: 501 });
+    } else {
+      console.log(user.$id);
+      const entries = await db.listDocuments("wahlen", "wahlen", [
+        Query.select(["name", "date", "owner", "type", "public", "access"]),
+        Query.equal("access", [user.$id]),
+      ]);
+      console.log(entries);
+      return Response.json(entries, { status: 200 });
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    console.error(error);
+    if (error.type === "user_jwt_invalid") {
+      return Response.json(error, { status: 401 });
+    } else {
+      return Response.json(
+        { type: error.type, message: error.message },
+        { status: 501 }
+      );
+    }
+  }
 }
 
 export async function POST(request: Request) {
-  console.log("POST request received");
   const data = await request.headers;
-  console.log("Request headers fetched");
   const client = new Client()
     .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
     .setProject("athesv") // Your project ID
     .setJWT(data.get("token") || "");
-  console.log("Client initialized with JWT token");
   const teams = new Teams(client);
 
-  const accessCheckPromise = teams.list();
-  console.log("Access check promise initiated");
+  try {
+    const value = await teams.list();
+    if (
+      value.teams.some(
+        (team) => team.$id !== "admin" && team.$id !== "committee"
+      )
+    ) {
+      return Response.json({}, { status: 401 });
+    }
 
-  accessCheckPromise
-    .then(
-      async (value) => {
-        console.log("Access check promise resolved");
-        if (
-          value.teams.some(
-            (team) => team.$id === "admin" || team.$id === "committee"
-          )
-        ) {
-          console.log("User is part of admin or committee team");
-          const account = new Account(client);
+    const account = new Account(client);
+    const AccountData = await account.get();
+    const userId = AccountData.$id;
 
-          const AccountData = await account.get();
-          console.log("Account data fetched");
+    const db = new Databases(client);
+    const name = data.get("name");
+    const date = data.get("date");
 
-          const userId = AccountData.$id;
+    if (!name || !date) {
+      return Response.json(
+        {},
+        { status: 400, statusText: "Required Data is Missing" }
+      );
+    }
 
-          const db = new Databases(client);
+    const type = data.get("type");
+    const id = ID.unique();
 
-          const name = data.get("name");
-          const date = data.get("date");
-
-          if (!name || !date) {
-            console.log("Required data is missing");
-
-            return Response.json(
-              {},
-              { status: 400, statusText: "Required Data is Missing" }
-            );
-          }
-
-          const type = data.get("type");
-          const id = ID.unique();
-
-          const dbPromise = db.createDocument("wahlen", "wahlen", id, {
-            name: name,
-            date: date,
-            owner: userId,
-          });
-          console.log("Database promise initiated for document creation");
-
-          dbPromise
-            .then(
-              async (data) => {
-                console.log("Database promise resolved for document creation");
-                if (type) {
-                  const updatePromise = db.updateDocument(
-                    "wahlen",
-                    "wahlen",
-                    id,
-                    {
-                      type: type,
-                    }
-                  );
-                  console.log("Update promise initiated for document type");
-                  updatePromise
-                    .then(
-                      (data) => {
-                        console.log(
-                          "Update promise resolved for document type"
-                        );
-                        return Response.json(data, { status: 201 });
-                      },
-                      (reason) => {
-                        console.error(
-                          "Update promise rejected for document type",
-                          reason
-                        );
-                        throw reason;
-                      }
-                    )
-                    .catch((reason) => {
-                      console.error(
-                        "Update promise caught error for document type",
-                        reason
-                      );
-                      throw reason;
-                    });
-                } else {
-                  console.log("No type provided, skipping update");
-                  return Response.json(data, { status: 201 });
-                }
-              },
-              (reason) => {
-                console.error(
-                  "Database promise rejected for document creation",
-                  reason
-                );
-                throw reason;
-              }
-            )
-            .catch((reason) => {
-              console.error(
-                "Database promise caught error for document creation",
-                reason
-              );
-              throw reason;
-            });
-        } else {
-          console.log("User is not part of admin or committee team");
-          return Response.json({}, { status: 401 });
-        }
-      },
-      (reason) => {
-        console.error("Access check promise rejected", reason);
-        if (reason.type == "user_jwt_invalid") {
-          console.log("JWT token is invalid");
-          return Response.json({}, { status: 401 });
-        } else {
-          console.error("Unknown error in access check promise", reason);
-          throw reason;
-        }
-      }
-    )
-    .catch((error) => {
-      console.error("Access check promise caught error", error);
-      return Response.json({}, { status: 501 });
+    const dbData = await db.createDocument("wahlen", "wahlen", id, {
+      name: name,
+      date: date,
+      owner: userId,
     });
+
+    if (type) {
+      await db.updateDocument("wahlen", "wahlen", id, { type: type });
+    }
+    return Response.json(dbData, { status: 201 });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    console.error(error);
+    if (error.type === "user_jwt_invalid") {
+      return Response.json(error, { status: 401 });
+    } else if (error.type == "document_invalid_structure") {
+      return Response.json(error, { status: 422 });
+    } else {
+      return Response.json(error, { status: error.code });
+    }
+  }
 }
